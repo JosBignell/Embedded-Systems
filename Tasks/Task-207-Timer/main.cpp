e34cfe2e74316310e58bd980aa9f1f8daec5057f
JosBignell <josiah.bignell@students.plymouth.ac.uk>
2022-10-07 15:42:22 +0100

    Taskedit2

--- Tasks/Task-207-Timer/main.cpp
+++ Tasks/Task-207-Timer/main.cpp
@@ -17,7 +17,7 @@ int main()
 {
     //Time how long it takes to perform a printf
     tmr1.start();
-    printf("Hello World!\n");
+    printf("Hello World How Are You!\n");
     tmr1.stop();
 
     //Print out how long it took
@@ -31,16 +31,16 @@ int main()
         while (SW_BLUE == 0);
 
         //Turn on LED
-        ledRed = 1;
+        //ledRed = 1;
 
         //Wait for 500ms
-        tmr1.reset();
-        while (tmr1.elapsed_time() < 500ms); //How about this for C++ magic :)
+        //tmr1.reset();
+        //while (tmr1.elapsed_time() < 500ms); //How about this for C++ magic :)
 
         wait_us(500000);
 
         //Turn off LED
-        ledRed = 0;
+        ledRed = !ledRed;
     }
 }
 
