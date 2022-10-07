169df772dd0657e825ab704854b1bd9d991a1193
JosBignell <josiah.bignell@students.plymouth.ac.uk>
2022-10-07 10:44:13 +0100

    Taskedits

--- Tasks/TASK-203-PortOut/main.cpp
+++ Tasks/TASK-203-PortOut/main.cpp
@@ -9,12 +9,19 @@
 //BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
 PortOut leds(PortC, LEDMASK);
 
+#define TRAF_GRN2_PIN PB_0
+#define TRAF_YEL2_PIN PB_7
+#define TRAF_RED2_PIN PB_14
+#define LEDMASK2 0b0100000010000001
+
+PortOut leds2(PortB, LEDMASK2);
+
 int main()
 {
     while (true) {
-        leds = 0;   //Binary 000
+        leds = leds^LEDMASK;   //Binary 000
         wait_us(500000);
-        leds = 0xFF;   //Binary 11111111
+        leds2 = leds2^LEDMASK2;   //Binary 11111111
         wait_us(500000);    
     }
 }
